659b5b0434b0c1c3d89fa27b0056b40bc9bd420e
Jesse Thomas <jgthomas3@gmail.com>
2023-05-13 11:40:55 -0600

    Feat: Add 3 routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/areas", to: "areas#index"
+  get "/climbs", to: "climbs#index"
+  get "/areas/:id", to: "areas#show"
+  get "/climbs/:id", to: "climbs#show"
 end
