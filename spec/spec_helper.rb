963e2af0cf9d2fe50fa8569280f83ffa2cae8064
Jesse Thomas <jgthomas3@gmail.com>
2023-05-13 19:42:59 -0600

    Test: Add test_data method, not working yet

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -92,3 +92,19 @@ RSpec.configure do |config|
   Kernel.srand config.seed
 =end
 end
+
+def test_data
+  @canal_zone = Area.create!(name: "Canal Zone", town: "Golden, CO", camping: false, trad: true, sport: true, parking_spaces: 70)
+  @high_wire = Area.create!(name: "High Wire", town: "Golden, CO", camping: false, trad: true, sport: true, parking_spaces: 16)
+  @cactus_cliff = Area.create!(name: "Cactus Cliff", town: "Canyon City, CO", camping: true, trad: true, sport: true, parking_spaces: 0)
+
+  @box_of_rain = Climb.create!(name: "Box of Rain", grade: "5.8", trad: false, sport: true, number_bolts: 8, area_id: @canal_zone.id)
+  @ripple = Climb.create!(name: "Ripple", grade: 5.8, trad: false, sport: true, number_bolts: 9, area_id: @canal_zone.id)
+  @route_canal = Climb.create!(name: "Route Canal", grade: "5.8+", trad: true, sport: false, number_bolts: 1, area_id: @canal_zone.id )
+
+  @deuces_wild = Climb.create!(name: "Deuces Wild", grade: "5.10a", trad: false, sport: true, number_bolts: 7, area_id: @high_wire.id)
+  @peoples_choice = Climb.create!(name: "People's Choide", grade: "5.10d", trad: false, sport: true, number_bolts: 20, area_id: @high_wire.id)
+
+  @the_stand = Climb.create!(name: "The Stand", grade: "5.10b/c", trad: false, sport: true, number_bolts: 7, area_id: @cactus_cliff.id)
+  @century_man = Climb.create!(name: "20th Century Man", grade: "5.10a/b", trad: false, sport: true, number_bolts: 7, area_id: @cactus_cliff.id)
+end
