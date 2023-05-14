fae79d2c1a8c9364974e3931f0c603996e91ec13
Jesse Thomas <jgthomas3@gmail.com>
2023-05-13 19:42:18 -0600

    Test: Add type: :feature, and bang!

--- spec/features/areas/show_spec.rb
+++ spec/features/areas/show_spec.rb
@@ -1,13 +1,13 @@
 require "rails_helper"
 
-RSpec.describe "the area show page" do
+RSpec.describe "the area show page", type: :feature do
   # User Story 2, Parent Show
   # As a visitor
   # When I visit '/parents/:id'
   # Then I see the parent with that id including the parent's attributes
   # (data from each column that is on the parent table)
   describe 'as a visitor, when I visit /areas/:id' do
-    let!(:canal_zone) { Area.create(name: "Canal Zone", town: "Golden, CO", camping: false, trad: true, sport: true, parking_spaces: 70) }
+    let!(:canal_zone) { Area.create!(name: "Canal Zone", town: "Golden, CO", camping: false, trad: true, sport: true, parking_spaces: 70) }
 
     it "displays the area name, area id and attributes" do
       visit "/areas/#{canal_zone.id}"
@@ -20,6 +20,9 @@ RSpec.describe "the area show page" do
       expect(page).to have_content(canal_zone.sport)
       expect(page).to have_content(canal_zone.parking_spaces)
       save_and_open_page
+
+      # make another area and test it
+
     end
 
   end
