1dd246579850380e2e628346a1503a1b18ba9771
Jesse Thomas <jgthomas3@gmail.com>
2023-05-14 17:47:26 -0600

    Test: add 2 tests, count & link/climbs

--- spec/features/areas/show_spec.rb
+++ spec/features/areas/show_spec.rb
@@ -9,6 +9,13 @@ RSpec.describe "the area show page", type: :feature do
   before(:each) do
     @canal_zone = Area.create!(name: "Canal Zone", town: "Golden, CO", camping: false, trad: true, sport: true, parking_spaces: 70)
     @high_wire = Area.create!(name: "High Wire", town: "Golden, CO", camping: false, trad: true, sport: true, parking_spaces: 16)
+
+    @deuces_wild = @high_wire.climbs.create!(name: "Deuces Wild", grade: "5.10a", trad: false, sport: true, number_bolts: 7)
+    @peoples_choice = @high_wire.climbs.create!(name: "People's Choice", grade: "5.10d", trad: false, sport: true, number_bolts: 20)
+
+    @box_of_rain = @canal_zone.climbs.create!(name: "Box of Rain", grade: "5.8", trad: false, sport: true, number_bolts: 8)
+    @ripple = @canal_zone.climbs.create!(name: "Ripple", grade: 5.8, trad: false, sport: true, number_bolts: 9)
+    @route_canal = @canal_zone.climbs.create!(name: "Route Canal", grade: "5.8+", trad: true, sport: false, number_bolts: 1)
   end
 
   describe 'as a visitor, when I visit /areas/:id' do
@@ -35,6 +42,37 @@ RSpec.describe "the area show page", type: :feature do
       expect(page).to have_content("Trad: #{@high_wire.trad}")
       expect(page).to have_content("Sport: #{@high_wire.sport}")
       expect(page).to have_content("Parking Spaces: #{@high_wire.parking_spaces}")
+    end
+
+    # User Story 7, Parent Child Count
+
+    # As a visitor
+    # When I visit a parent's show page
+    # I see a count of the number of children associated with this parent
+    # Usability
+
+    it 'displays a count of the number of children associated with the parent' do
+      visit "/areas/#{@high_wire.id}"
+      expect(page).to have_content("Number of Climbs: #{@high_wire.climbs.count}")
+      # save_and_open_page
+
+      visit "/areas/#{@canal_zone.id}"
+      expect(page).to have_content("Number of Climbs: #{@canal_zone.climbs.count}")
+      # save_and_open_page
+    end
+
+    it 'links to climbs index from any show page' do
+      visit "/areas/#{@high_wire.id}"
+      save_and_open_page
+      expect(page).to have_content("Go To Climbs Index")
+      click_on "Go To Climbs Index"
+      expect(current_path).to eq("/climbs")
+      save_and_open_page # this opens the page linked to
+
+      visit "/areas/#{@canal_zone.id}"
+      expect(page).to have_content("Go To Climbs Index")
+      click_on "Go To Climbs Index"
+      expect(current_path).to eq("/climbs")
     end
 
   end
