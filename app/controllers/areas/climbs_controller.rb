d716537dfeb7cc79cbd63c85c8e21ef71220756d
Jesse Thomas <jgthomas3@gmail.com>
2023-05-15 22:15:02 -0600

    Refactor: removed area from params id

--- app/controllers/areas/climbs_controller.rb
+++ app/controllers/areas/climbs_controller.rb
@@ -1,7 +1,7 @@
 class Areas::ClimbsController < ApplicationController
   # this controller is going to be associated with every climb inside a specific area
   def index
-    @area = Area.find(params[:area_id])
+    @area = Area.find(params[:id])
     @climbs = @area.climbs
   end
 end
